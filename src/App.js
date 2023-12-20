b4dfdfe629a7f9eca185c16f8aac1dcf75c103a9
MD Shaharia <adeptshaharia@gmail.com>
2023-12-20 15:45:20 +0600

    Dextop_done

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { FaHistory } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiFillCaretUp } from "react-icons/ai";
+import { FaCheckSquare } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import { useState } from "react";
 import light from "../src/Images/Toggle button.png";
 import dark from "../src/Images/Toggle button (1).png";
 import simple1 from "../src/Images/Svg_sample1.png";
+import simple2 from "../src/Images/Svg_sample2.png";
 import avatar from "../src/Images/Avatar.png";
 import love from "../src/Images/Frame 2846.png";
 import love1 from "../src/Images/Frame 2846 (3).png";
@@ -109,7 +111,7 @@ function App() {
           </div>
           <div className="absolute bottom-28  w-full flex justify-center">
             {hide ? (
-              <p className="text-16px  bg-gradient-to-r w-auto from-[#FF797B]  to-[#FF797B] px-8 py-3 text-white ">
+              <p className="text-16px  bg-gradient-to-r w-auto from-[#FF797B] rounded-md to-[#FF797B] px-8 py-3 text-white ">
                 New appointment
               </p>
             ) : (
@@ -127,8 +129,8 @@ function App() {
         </div>
       </div>
 
-      <div className=" dark:bg-[#150A09] dark:text-white bg-[#F9F9F9] w-full pl-8 pr-2 pt-10 ">
-        <div className="flex justify-between items-center  h-24">
+      <div className=" dark:bg-[#150A09] dark:text-white bg-[#F9F9F9] w-full pl-10 pr-7 pt-5 ">
+        <div className="flex justify-between items-center  h-24 py-18">
           <p className="text-[24px] text-[#646F75] dark:text-white">Home</p>
           <div className="flex justify-center gap-3 ">
             <div className="flex justify-center items-center">
@@ -167,7 +169,7 @@ function App() {
                  </div>
             </article>
             <div className="grid grid-cols-4 py-10 gap-4">
-            <article className="shadow-md px-5 py-2 dark:bg-[#2B2121]">
+            <article className="shadow-md rounded-md px-5 py-2 dark:bg-[#2B2121]">
               <div>
                 <img  alt='love' src={love} />
               </div>
@@ -179,7 +181,7 @@ function App() {
               <p className="text-green-500">Normal</p>
                 </div>
             </article>
-            <article className="shadow-md px-5 py-2 dark:bg-[#2B2121]">
+            <article className="shadow-md rounded-md px-5 py-2 dark:bg-[#2B2121]">
               <div>
                 <img  alt='love' src={love1} />
               </div>
@@ -192,7 +194,7 @@ function App() {
                 <p className="text-[#82AB0D]">Slightly higher</p>
                 </div>
             </article>
-            <article className="shadow-md px-5 py-2 dark:bg-[#2B2121]">
+            <article className="shadow-md rounded-md px-5 py-2 dark:bg-[#2B2121]">
               <div>
                 <img  alt='love' src={love2} />
               </div>
@@ -205,7 +207,7 @@ function App() {
                 <p className="text-[#82AB0D]">Slightly higher</p>
                 </div>
             </article>
-            <article className="shadow-md px-5 py-2 dark:bg-[#2B2121]">
+            <article className="shadow-md rounded-md px-5 py-2 dark:bg-[#2B2121]">
               <div>
                 <img  alt='love' src={love3} />
               </div>
@@ -220,12 +222,169 @@ function App() {
             </article>
           
             </div>
+
+            {/* Todo List  */}
+            <div className="bg-white dark:bg-[#2c2121] px-5 py-3">
+              <h3 className="pb-4 text-2xl">To-Do List</h3>
+              <article className="flex justify-between gap-x-2">
+                <textarea className="w-10/12"></textarea>
+                <div className="">
+                <button className="bg-[#FF797B] w-28 py-1 font-medium text-white rounded-md">Add</button>
+                </div>
+                
+              </article>
+              <article className="flex justify-between gap-x-2 mt-8">
+                
+                 <div className="flex justify-center items-center"><FaCheckSquare className="text-[#EDEBEB] text-3xl mr-3" />
+                 <p>Select All</p></div>
+                 <div><button className="bg-[#FF797B] w-28 py-1 font-medium text-white rounded-md">Done</button></div>
+              </article>
+              <article className="mt-10">
+                <ul>
+                  <li className="flex justify-between items-center py-1">
+                    <div className="flex justify-center items-center">
+                    <FaCheckSquare className="text-[#FF797B] text-3xl mr-3" />
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    </div>
+                 
+                  <p className="text-[#E0E0E0]">Last Added: 10 sep 2022 </p>
+                  </li>
+                  <li className="flex justify-between items-center py-1">
+                    <div className="flex justify-center items-center">
+                    <FaCheckSquare className="text-[#E0E0E0] text-3xl mr-3" />
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    </div>
+                 
+                  <p className="text-[#E0E0E0]">Last Added: 10 sep 2022 </p>
+                  </li>
+                  <li className="flex justify-between items-center py-1">
+                    <div className="flex justify-center items-center">
+                    <FaCheckSquare className="text-[#E0E0E0] text-3xl mr-3" />
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    </div>
+                 
+                  <p className="text-[#E0E0E0]">Last Added: 10 sep 2022 </p>
+                  </li>
+                  <li className="flex justify-between items-center py-1">
+                    <div className="flex justify-center items-center">
+                    <FaCheckSquare className="text-[#E0E0E0] text-3xl mr-3" />
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    </div>
+                 
+                  <p className="text-[#E0E0E0]">Last Added: 10 sep 2022 </p>
+                  </li>
+                
+                </ul>
+              </article>
+
+            </div>
           
   
           </div>
-          <div className="col-span-5 bg-red-500">
-            <article>asdvfasdf</article>
-            <article>asdfasdfasdfa</article>
+          <div className="col-span-5 ">
+            <article>
+            <div className="bg-white dark:bg-[#2c2121] px-5 py-3">
+              <h3 className="pb-4 text-2xl">Upcoming-Events</h3>
+           
+              <article className="mt-5">
+                <ul>
+                  <li className="flex justify-between items-center dark:bg-[#6a5050]  bg-[#FDDAD6] rounded-lg border mt-5">
+                    <div className="flex justify-center items-center">
+                    <div className="bg-white dark:bg-[#2c2121] px-3 py-2 rounded-md">
+                      <p>2022</p>
+                      <p>23</p>
+                      <p>Sep</p>
+                    </div>
+                    <div className="px-2 py-1">
+                    <p className="font-medium">Dr. Muhammad Abdul Hussein </p>
+                    <p>Cardiologist </p>
+                    <div className="flex gap-2 oy-4">
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md"> Start Morning</button>
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md">Time 10:00 AM</button>
+                    </div>
+                    </div>
+                  
+                    </div>
+                
+                  </li>
+                  <li className="flex justify-between items-center dark:bg-[#6a5050]  bg-[#FDDAD6] rounded-lg border mt-5">
+                    <div className="flex justify-center items-center">
+                    <div className="bg-white dark:bg-[#2c2121] px-3 py-2 rounded-md">
+                      <p>2022</p>
+                      <p>23</p>
+                      <p>Sep</p>
+                    </div>
+                    <div className="px-2 py-1">
+                    <p className="font-medium">Dr. Muhammad Abdul Hussein </p>
+                    <p>Cardiologist </p>
+                    <div className="flex gap-2 oy-4">
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md"> Start Morning</button>
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md">Time 10:00 AM</button>
+                    </div>
+                    </div>
+                  
+                    </div>
+                
+                  </li>
+                  <li className="flex justify-between items-center dark:bg-[#6a5050]  bg-[#FDDAD6] rounded-lg border mt-5">
+                    <div className="flex justify-center items-center">
+                    <div className="bg-white dark:bg-[#2c2121] px-3 py-2 rounded-md">
+                      <p>2022</p>
+                      <p>23</p>
+                      <p>Sep</p>
+                    </div>
+                    <div className="px-2 py-1">
+                    <p className="font-medium">Dr. Muhammad Abdul Hussein </p>
+                    <p>Cardiologist </p>
+                    <div className="flex gap-2 oy-4">
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md"> Start Morning</button>
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md">Time 10:00 AM</button>
+                    </div>
+                    </div>
+                  
+                    </div>
+                
+                  </li>
+                  <li className="flex justify-between items-center dark:bg-[#6a5050]  bg-[#FDDAD6] rounded-lg border mt-5">
+                    <div className="flex justify-center items-center">
+                    <div className="bg-white dark:bg-[#2c2121] px-3 py-2 rounded-md">
+                      <p>2022</p>
+                      <p>23</p>
+                      <p>Sep</p>
+                    </div>
+                    <div className="px-2 py-1">
+                    <p className="font-medium">Dr. Muhammad Abdul Hussein </p>
+                    <p>Cardiologist </p>
+                    <div className="flex gap-2 oy-4">
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md"> Start Morning</button>
+                      <button className="bg-white dark:bg-[#2c2121] px-2 p-1 rounded-md">Time 10:00 AM</button>
+                    </div>
+                    </div>
+                  
+                    </div>
+                
+                  </li>
+              
+                 
+                </ul>
+              </article>
+
+            </div>
+            </article>
+            <article className="px-5 bg-white dark:bg-[#2c2121] mt-3">
+              <div className="flex justify-between py-4 px-2"><p>Covid-19 Updates</p>
+              <p>T10 September 2022<br/> AM Thursday  10:00:00 AM</p>
+              </div>
+            <article className="bg-gradient-to-r  flex px-2 ">
+                 <div><img className=" h-[270px] w-[270px]"  alt="doctor" src={simple2}/></div>
+                 <div className="flex justify-center items-center text-left bg-gradient-to-r text-white from-[#FF797B] via-[#FF7C65] to-[#FF7C65] flex-col w-full ">
+                <h3 className="text-2xl text-center">Infection Number <br/><span>500</span></h3>
+                <h3 className="text-2xl text-center mt-4">Infection Ratio <br/><span>10%</span></h3>
+    
+                  
+                 </div>
+            </article>
+            </article>
           </div>
         </div>
       </div>
